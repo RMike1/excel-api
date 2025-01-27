876dc391acd56298454e4850b5c75c2e947e1b7c
RMike1 <71224316+RMike1@users.noreply.github.com>
2025-01-27 15:07:39 +0200

    adding export job class

--- tests/Feature/GenerateExcelFileTest.php
+++ tests/Feature/GenerateExcelFileTest.php
@@ -7,7 +7,6 @@ it('generates excel file', function(){
     $spy=$this->spy(FileService::class);
     $this->postJson('api/employees')
     ->assertStatus(200)
-    ->assertHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    ->assertHeader('Content-Disposition', 'attachment; filename="' . 'employees.xlsx"');  
+    ->assertHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     $spy->shouldHaveReceived()->generateExcel();
 });
