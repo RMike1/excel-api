5d5ce5acfabc606fed2d36af83cff038100e74aa
RMike1 <71224316+RMike1@users.noreply.github.com>
2025-02-05 18:19:33 +0200

    feat adding new feature for displaying employees data from storage..

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use App\Services\AllEmployees;
 use App\Http\Controllers\Controller;
 use App\Services\Reports\FileService;
 use Illuminate\Support\Facades\Storage;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class EmployeeController extends Controller
 {
-    public function __invoke(FileService $excel)
+    public function export(FileService $excel)
     {
             $filePath = $excel->generateExcel();
             if (Storage::exists($filePath)) {
@@ -24,4 +26,22 @@ class EmployeeController extends Controller
     
             return response()->json(['message' => 'File generation is in progress...'], 202);
     }
+
+    public function allEmployees(AllEmployees $allEmployees)
+    {
+        return response()->json([
+            $allEmployees->employees(),
+        ]);
+    }
+    public function allEmployeesFromStorage(Request $request, AllEmployees $allEmployeesFromStorage)
+    {
+        $file = Storage::disk('local')->path('exports/employees_20250205131505.xlsx');
+        $page = $request->query('page', 1);
+        $perPage = 50;
+        
+        $employees= $allEmployeesFromStorage->employeesFromStorage($file, $perPage, $page);
+
+        return response()->json($employees);
+}
+
 }
--- app/Services/Reports/FileService.php
+++ app/Services/Reports/FileService.php
@@ -6,6 +6,7 @@ use App\Jobs\ExportEmployeesJob;
 
 
 
+
 class FileService
 {
     /**
